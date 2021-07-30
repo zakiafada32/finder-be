c3dfeb0d16e52803d16fafa2b194a864029083df
zakiafada32 <afadazaki32@gmail.com>
2021-07-30 22:53:19 +0700

    debugger

--- lib/finder-be-stack.ts
+++ lib/finder-be-stack.ts
@@ -6,6 +6,7 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/lib/aws-apigateway';
 import { GenericTable } from './generic-table';
 import { NodejsFunction } from 'aws-cdk-lib/lib/aws-lambda-nodejs';
 import { handler } from '../services/node-lambda/hello';
+import { PolicyStatement } from 'aws-cdk-lib/lib/aws-iam';
 export class FinderBeStack extends Stack {
   private api = new RestApi(this, 'FinderApi');
   private finderTable = new GenericTable('FinderTable', 'spaceId', this);
@@ -13,18 +14,18 @@ export class FinderBeStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     // The code that defines your stack goes here
-    const helloLambda = new LambdaFunction(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      handler: 'hello.main',
-    });
 
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler',
     });
 
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*');
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
     const helloLambdaResource = this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,9 +1,20 @@
 import { v4 } from 'uuid';
+import { S3 } from 'aws-sdk';
+
+const s3Client = new S3();
 
 async function handler(event: any, context: any) {
+  console.log('event log:');
+  console.log(event);
+
+  console.log('context log:');
+  console.log(context);
+
+  const buckets = await s3Client.listBuckets().promise();
+
   return {
     statusCode: 200,
-    body: 'Hello from lambda!' + v4(),
+    body: 'Hello from lambda: ' + JSON.stringify(buckets.Buckets),
   };
 }
 
