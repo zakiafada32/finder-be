b079ff0f1cc76b8f727901f2023792f81f789e0d
zakiafada32 <afadazaki32@gmail.com>
2021-07-30 19:13:57 +0700

    lambda and dynamo db

--- lib/finder-be-stack.ts
+++ lib/finder-be-stack.ts
@@ -1,10 +1,24 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/lib/aws-lambda';
+import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/lib/aws-apigateway';
+import { GenericTable } from './generic-table';
 export class FinderBeStack extends Stack {
+  private api = new RestApi(this, 'FinderApi');
+  private finderTable = new GenericTable('FinderTable', 'spaceId', this);
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
     // The code that defines your stack goes here
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main',
+    });
+
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
 }
